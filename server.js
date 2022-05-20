79786a6a3d1293a04abd2f515de11fc18cfff370
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-21 05:08:48 +0800

    add encrypted session key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 //const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+require('dotenv').config();
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.ENCRYPED_SESSION_KEY,
   cookie: {},
   resave: false,
   saveUninitialized: true,
