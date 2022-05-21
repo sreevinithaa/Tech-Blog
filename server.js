159021847b17dade9178e1c832539e9c5ae508ef
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-21 11:07:39 +0800

    update layout and login page

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.ENCRYPED_SESSION_KEY,
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
