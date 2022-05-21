4f150ce876de75b99ff7bb47c2e5113593b53337
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-21 20:06:14 +0800

    update login and signup functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,31 @@ const router = require('express').Router();
 const { Comment,Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+router.get('/', withAuth, async (req, res) => {
+    try {
+      // Get all blog and JOIN with user data
+      const blogData = await Blog.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+  console.log(blogs);
+      // Pass serialized data and session flag into template
+      res.render('home', { 
+        blogs, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
