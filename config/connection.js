d95c1f191aff4dad2a161a01b04cdd351d2753f7
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-25 06:04:02 +0800

    Update connection

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    'sql6494872',
-    'sql6494872',
-    'h4twgdLBjf',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: 'sql6.freemysqlhosting.net',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
